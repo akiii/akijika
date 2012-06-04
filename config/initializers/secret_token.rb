7fd0c7fb1f04efdf5915b4b5d19ba21fc8296ee4
akiii <akiii@sfc.wide.ad.jp>
2012-06-05 01:30:44 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Akijika::Application.config.secret_token = 'fa6cac521e6586b9087e2ff2e87e1923e345ecd4fa8de1b3e37be177d7e616bcc4a4fc08f39a16f9945ffca62f859a3d0b4955ea6262bcef6f8a72b192caa28d'
